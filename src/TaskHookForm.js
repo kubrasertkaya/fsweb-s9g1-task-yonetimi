4b4ac078aea0739c7a7140fb7035717fe87fae12
kubrasertkaya <118736856+kubrasertkaya@users.noreply.github.com>
2023-05-16 01:39:24 +0300

    React-hook-form kütüphanesi.

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,86 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
+export default function TaskHookForm({ kisiler, submitFn }) {
+ const {
+  register,
+  handleSubmit,
+  formState:{errors}
 
-export default function TaskHookForm() {
-  return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+  } =useForm({
+  mode:"onChange",
+
+ });
+ const theSubmit=(data)=>console.log(data);
+ return(
+  <form className="taskForm" onSubmit={handleSubmit(theSubmit)}> 
+  <div className="form-line">
+        <label className="input-label" htmlFor="title">
+          Başlık
+        </label>
+        <input
+          className="input-text"
+          id="title"
+         {...register("title",{required:"Task başlığı yazmalısınız"})}
+          type="text"
+         
+         
+        />
+        {errors.title && <p className="input-error">{errors?.title?.message}</p>}
+      </div>
+
+      <div className="form-line">
+        <label className="input-label" htmlFor="description">
+          Açıklama
+        </label>
+        <textarea
+          className="input-textarea"
+          rows="3"
+          id="description"
+         {...register("description",{
+          required:"Task açıklaması yazmalısınız",
+          minLength:{
+            value:10,
+            message:"Task açıklaması en az 10 karakter olmalı",
+          }
+        })}
+         
+          
+        ></textarea>
+       {errors.description && <p className="input-error">{errors?.description?.message}</p>}
+      </div>
+
+      <div className="form-line">
+        <label className="input-label">İnsanlar</label>
+        <div>
+          {kisiler.map((p) => (
+            <label className="input-checkbox" key={p}>
+              <input
+                type="checkbox"
+               {...register("people",{
+                required:"Lütfen en az bir kişi seçin",
+                validate: peopleList => peopleList.length <=3 || "En fazla 3 kişi seçebilirsiniz" 
+              })}
+                value={p}
+                
+               
+              />
+              {p}
+            </label>
+          ))}
+        </div>
+       { errors.people && <p className="input-error">{errors?.people?.message}</p>}
+      </div>
+
+      <div className="form-line">
+        <button
+          className="submit-button"
+          type="submit"
+          // disabled={buttonDisabled}
+        >
+          Kaydet
+        </button>
+      </div>
+  </form>
+ )
+   
 }
